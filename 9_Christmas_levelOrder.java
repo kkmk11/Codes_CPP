7a360b84f52be862f35df897bc9d90db1caec4ea
Mahesh Kumar <88311960+kkmk11@users.noreply.github.com>
2023-02-02 14:24:01 +0530

    Update 9_Christmas_levelOrder.java

--- 9_Christmas_levelOrder.java
+++ 9_Christmas_levelOrder.java
@@ -1,3 +1,64 @@
+// For X-Mas, santa claus is preparing a X-Mas Tree with set of Bulbs.
+// The bulbs are of different voltages, and preparation of tree as follows:
+// 	- The bulbs are arranged in level-wise, levels are numbered from 0,1,2,3..
+// 	  so on.
+// 	- At level-0: There will be only one bulb as root bulb.,
+// 	- From next level onwards, we can attach atmost two bulbs to left side,
+// 	  and right side of every bulb in previous level.
+// 	- The empty attachements in a level are indicated with -1. 
+// 	(for example: look in hint)
+	
+// Entire X-Mas tree has to be prepared with certian rules as follows:
+// 	- For every even level in the X-Mas Tree, all the bulbs should have
+// 	  odd voltages in strictly ascending order.
+// 	- For every odd level in the X-Mas Tree, all the bulbs should have
+// 	  even voltages in strictly descending order.
+	
+// You will be given the X-Mas Tree root,
+// Your task is to findout whether the X-Mas tree is prepared as per the rules
+// or not.
+
+// Implement the class Solution.
+// 1.public boolean isXmasTree(BinaryTreeNode root): returns a boolean value.
+
+
+// Input Format:
+// -------------
+// A single line of space separated integers, voltages of the set of bulbs.
+
+// Output Format:
+// --------------
+// Print a boolean value.
+
+
+
+// Sample Input-1:
+// ---------------
+// 3 8 4 3 5 -1 7 
+
+// Sample Output-1:
+// ----------------
+// true
+
+
+// Sample Input-2:
+// ---------------
+// 3 8 4 3 5 7 7 
+
+// Sample Output-2:
+// ----------------
+// false
+
+
+// Sample Input-3:
+// ---------------
+// 1
+
+// Sample Output-3:
+// ----------------
+// true
+/*
+
 /*
 //TreeNode Structure for Your Reference..
 
