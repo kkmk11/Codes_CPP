203163d14877564ffdca1ed9951835eba9087ef2
Mahesh Kumar <88311960+kkmk11@users.noreply.github.com>
2023-02-04 07:15:51 +0530

    Update 15_HouseRobber.cpp

--- 15_HouseRobber.cpp
+++ 15_HouseRobber.cpp
@@ -58,3 +58,10 @@ int main(){
         cin>>v[i];
     }
     vector<int> dp(n,0);
+    dp[0]=v[0];
+    dp[1]=max(v[0],v[1]);
+    for(int i=2;i<n;i++){
+        dp[i]=max(dp[i-2]+v[i],dp[i-1]);
+    }
+    cout<<max(dp[n-1],dp[n-2]);
+}
