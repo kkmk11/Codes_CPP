ceb0a713bc6859b03d20e7029c4bae3887012d7a
Mahesh Kumar <88311960+kkmk11@users.noreply.github.com>
2023-07-06 15:47:03 +0530

    Create KeyBoard_Sequence.cpp

---
+++ KeyBoard_Sequence.cpp
@@ -0,0 +1,69 @@
+// Amogh is an Antiquarian, The person who collects antiques.
+// He found a rear keyboard which has following keys,
+// Keys are 'N', 'S', 'C' and 'P'
+
+// 1st Key - 'N': Print one character 'N' on Console.
+// 2nd Key - 'S': Select the whole Console.
+// 3rd Key - 'C': Copy selected content to buffer.
+// 4th Key - 'P': Print the buffer on Console, and append it after what has 
+// already been printed.
+
+// Now, your task is to find out maximum numbers of 'N's you can print
+// after K keystrokes . 
+
+// Input Format:
+// -------------
+// An integer K
+
+// Output Format:
+// --------------
+// Print an integer, maximum numbers of 'N's you can print.
+
+
+// Sample Input-1:
+// -------------------
+// 3
+
+// Sample Output-1:
+// -------------------- 
+// 3
+
+// Explanation: 
+// ---------------
+// We can print at most get 3 N's on console by pressing following key sequence:
+// N, N, N
+
+
+
+// Sample Input-2:
+// -------------------
+// 7
+
+// Sample Output-2:
+// ---------------------
+// 9
+
+// Explanation: 
+// ---------------
+// We can print at most get 9 N's on console by pressing following key sequence:
+// N, N, N, S, C, P, P
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int> dp(n+1,0);
+    dp[0]=0;
+    for(int i=1;i<dp.size();i++){
+        dp[i]=dp[i-1]+1;
+        if(i>=2){
+            for(int j=2;j<=i;j++){
+                dp[i]=max(dp[i],dp[i-j]*(j-1));
+            }
+        }
+    }
+    cout<<dp[n];
+}
